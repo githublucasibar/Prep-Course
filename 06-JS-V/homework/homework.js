1b362f9fcf1234eb8691c42e08718912f74fe8e1
githublucasibar <102768880+githublucasibar@users.noreply.github.com>
2022-04-12 19:21:34 -0300

    empece unidad 5

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -7,13 +7,26 @@ function crearUsuario() {
   // El `contructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 'Hola, mi nombre es {{nombre}}'
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
-  // Tu código:
+  // Tu código:,
+  function Usuario(opciones) {
+    this.usuario= opcion.usuario;
+    this.nombre= opcion.nombre;
+    this.email= opcionis.email;
+    this.password= opcion.password;
+   }
+  Usuario.prototype.saludar= function(){
+    return "Hola, mi "+this.nombre
+  }
+  return Usuario
 }
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+  Constructor.prototype.saludar=function(){
+    return "Hello World";
+  }
 }
 
 function agregarStringInvertida() {
@@ -22,12 +35,19 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
+String.prototype.reverse=function(){
+    for(i=this.length-1 ; i>=o ; i--)
+    var recontstruccion = []
+    reconstruccion.push(this[i]); 
+  }
+  return reconstruccion
 }
 
 // ---------------------------------------------------------------------------//
   //Crea el constructor de la clase "Persona"
   //Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio"
-  //Debe tener un método llamado "detalle" que nos devuelve un objeto con las propiedades de la persona y sus valores.
+  //Debe tener un método llamado "detalle" que nos devuelve un objeto con las propiedades de la persona y
+  // sus valores.
   //Ej: { 
     //   Nombre: 'Juan',
     //   Apellido: 'Perez',
