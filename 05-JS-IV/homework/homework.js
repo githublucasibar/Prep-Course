04304a78a9b36cbfe3b84ba5e686689be700c2b4
githublucasibar <102768880+githublucasibar@users.noreply.github.com>
2022-04-15 16:48:34 -0300

    texto 4

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -31,14 +31,15 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
-  objeto[metodo]=function(){}
+  objeto[metodo]();
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
-return objetoMisterioso[numeroMisterioso]*5;
+  var producto = objetoMisterioso[numeroMisterioso]*5;
+  return producto;
 }
 
 function eliminarPropiedad(objeto, unaPropiedad) {
